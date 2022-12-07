d7cfb5b13ea5ce29ad666e7af56c0d6bf4d926ca
Brian Jungmin Park <76887483+snowoflondon@users.noreply.github.com>
2022-12-07 14:26:08 -0500

    Update server.R
    Added functionality for player stat bar graphs

--- server.R
+++ server.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(rvest)
 library(ggrepel)
 library(DT)
+library(xml2)
+library(RColorBrewer)
 
 # fetch data from fbref
 url <- 'https://fbref.com/en/comps/9/Premier-League-Stats'
@@ -71,6 +73,44 @@ plot_descs <- list(
 
 `%nin%` <- Negate(`%in%`) # helper function for negation of %in% 
 
+ url_players <- list(
+  'https://fbref.com/en/comps/9/shooting/Premier-League-Stats',
+  'https://fbref.com/en/comps/9/passing/Premier-League-Stats',
+  'https://fbref.com/en/comps/9/gca/Premier-League-Stats',
+  'https://fbref.com/en/comps/9/defense/Premier-League-Stats'
+)
+
+# helper function to scrape dynamic tables
+pull_xml_table <- function(x){
+  df <- read_html(x) %>% xml_find_all('//comment()') %>% xml_text() %>% 
+    paste0(collapse = "") %>% read_html %>% xml_find_all("//table") %>% html_table
+  df <- df[[1]]
+  df <- clean_names(df)
+}
+
+df_players <- lapply(url_players, pull_xml_table)
+df_players <- df_players %>% lapply(function(x) x[,!duplicated(colnames(x))])
+key_stats <- c('Player', 'Pos', 'Gls', 'Sh', 'npxG', 
+               'Ast', 'xA', 'PrgDist', 'Att', 'Cmp%',
+               'SCA', 'TklW', 'Tkl%', 'Int', 'Clr')
+df_players <- df_players %>% lapply(function(x) x %>% select(any_of(key_stats)))
+df_players[-(1:2)] <- df_players[-(1:2)] %>% lapply(function(x) x %>%
+                                              select(-any_of(c('Sh', 'Att'))))
+df_players <- df_players %>% lapply(function(x) x %>% 
+                                      filter(Player != 'Player'))
+df_new <- df_players[[1]]
+for (i in 2:length(df_players)){
+  df_new <- df_new %>% inner_join(df_players[[i]])
+}
+df_new <- df_new[!duplicated(df_new$Player),]
+df_new <- df_new %>% 
+  mutate(Pos = strsplit(Pos, split = ',') %>% map(~ .[1]) %>% unlist())
+df_new <- df_new %>% filter(Pos != 'GK')
+df_new <- df_new %>% mutate(across(3:15, as.numeric))
+df_medians <- df_new %>% group_by(Pos) %>% 
+  summarise(across(3:14, function(x) median(x, na.rm = TRUE))) %>%
+  mutate(Player = 'League Median') %>% relocate(Player)                             
+                              
 # core server function
 function(input, output){
 
@@ -85,19 +125,17 @@ function(input, output){
         ]]))
   }) # output live updating table
   
-  output$plot <- renderPlot({
+output$plot <- renderPlot({
     var_y <- strsplit(as.character(input$plotSelect), ' vs. ') %>%
-      unlist() %>% head(1) # define y variable for ggplot
+      unlist() %>% head(1)
     var_x <- strsplit(as.character(input$plotSelect), ' vs. ') %>%
-      unlist() %>% tail(1) # define x variable for ggplot
-    
+      unlist() %>% tail(1)
     if (input$plotSelect %nin% c('GF vs. xG', 'GA vs. PSxG')){
       p <- ggplot(react_data(), aes(x = UQ(sym(var_x)),
                                     y = UQ(sym(var_y)))) + 
         geom_point(size = 6) +
         geom_point(data = react_data() %>% filter(Squad == input$teamSelect),
                    aes(color = Squad), size = 6) +
-        geom_label_repel(aes(label = Squad)) +
         theme_bw() +
         theme(text = element_text(size = 14), legend.position = 'none') + 
         geom_vline(
@@ -118,7 +156,6 @@ function(input, output){
         geom_point(size = 6) +
         geom_point(data = react_data() %>% filter(Squad == input$teamSelect),
                    aes(color = Squad), size = 6) +
-        geom_label_repel(aes(label = Squad)) + 
         theme_bw() +
         theme(text = element_text(size = 14), legend.position = 'none') +
         geom_abline() + 
@@ -130,17 +167,71 @@ function(input, output){
         geom_point(size = 6) +
         geom_point(data = react_data() %>% filter(Squad == input$teamSelect),
                    aes(color = Squad), size = 6) +
-        geom_label_repel(aes(label = Squad)) + 
         theme_bw() +
         theme(text = element_text(size = 14), legend.position = 'none') +
         geom_abline() + 
         labs(title = input$plotSelect, 
              subtitle = paste0(input$teamSelect, ' highlighted'))
     }
+    if (input$hideLabels == TRUE){ # checkbox selection 
+      p <- p + geom_label_repel(data = react_data() %>%
+                                  filter(Squad == input$teamSelect),
+                                aes(label = Squad))
+    } else {
+      p <- p + geom_label_repel(aes(label = Squad))
+    }
     print(p)
   })
   
   output$text <- renderText({
     plot_descs[[grep(input$plotSelect, all_vis)]] # description below plot
+  })
+  
+    # server functions for player stats tab
+  react_data2 <- eventReactive(input$searchSelect, {
+    df_new %>% filter(str_detect(Player, input$playerSelect)) %>% 
+      bind_rows(df_medians) %>% select(-Gls) %>%
+      group_by(Pos) %>% filter(n() != 1) %>% ungroup()
+  })
+  
+    output$playerPlot <- renderPlot({
+    validate( # in case player search is invalid, display error message
+      need(expr = nrow(react_data2()) == 2,
+           message = 'Search error! \nCheck for unique, valid name search 
+           \nFor all searchable names, visit the link on the sidebar'))
+    q <-  react_data2() %>% select(-Pos) %>%
+        pivot_longer(-Player, names_to = 'Metric', values_to = 'Values') %>%
+        mutate(Metric = case_when(Metric == 'Sh' ~ 'Shots',
+                                  Metric == 'npxG' ~ 'Non-penalty xG',
+                                  Metric == 'Ast' ~ 'Assists',
+                                  Metric == 'xA' ~ 'xA',
+                                  Metric == 'PrgDist' ~ 'Prg. Distance',
+                                  Metric == 'Att' ~ 'Passes Attempted',
+                                  Metric == 'Cmp%' ~ 'Pass Completion %',
+                                  Metric == 'SCA' ~ 'Shot Creating Actions',
+                                  Metric == 'TklW' ~ 'Tackles Won',
+                                  Metric == 'Tkl%' ~ 'Tackles Won %',
+                                  Metric == 'Int' ~ 'Interceptions',
+                                  Metric == 'Clr' ~ 'Clearances')) %>%
+        mutate(Metric = factor(Metric,
+                               levels = c('Shots', 'Non-penalty xG', 'Assists', 
+                                          'xA', 'Prg. Distance', 
+                                          'Passes Attempted',
+                                          'Pass Completion %',
+                                          'Shot Creating Actions',
+                                          'Tackles Won', 'Tackles Won %',
+                                          'Interceptions', 'Clearances'))) %>%
+        ggplot(aes(x = Player, y = Values)) + 
+        geom_bar(stat = 'identity', aes(fill = Player), alpha = .6) +
+        facet_wrap(~Metric, scales = 'free') + theme_bw() + 
+        theme(legend.position = 'bottom', text = element_text(size = 14),
+              axis.text.x = element_blank()) +
+        xlab('') + ylab('') +
+        labs(title = paste0(react_data2()$Player, 
+                            ' vs. League Median amongst ',
+                            react_data2()$Pos),
+             subtitle = 'Premier League') + 
+      scale_fill_brewer(palette = 'Set1')
+    print(q)
   })
 }
